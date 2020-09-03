f234552ab9adbf6de2683a7dbf2e7385809ff340
devAndrew00 <69908273+devAndrew00@users.noreply.github.com>
2020-09-03 22:10:37 +0100

    Add files via upload

---
+++ mod4_solution/script.js
@@ -0,0 +1,25 @@
+(function greetNames() {
+  var names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
+
+  for (let i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0);
+    firstLetter = firstLetter.toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
